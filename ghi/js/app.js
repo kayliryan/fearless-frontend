cd6c369b19c6024d52b8c6b5e1248c6e0367debd
Kayli <kayliryan@gmail.com>
2022-07-19 17:34:43 -0700

    Alert added and tested. Footer fixed. Wrap still not working

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -2,7 +2,7 @@
 function createCard(name, description, pictureUrl, start, end, location) {
     return `
     <div class = ".card-columns">
-      <div class="card shadow p-3 mb-5 bg-white rounded d-inline-flex p-2">
+      <div class="card shadow mb-5 bg-white rounded d-inline-flex">
         <img src="${pictureUrl}" class="card-img-top">
         <div class="card-body">
           <h5 class="card-title">${name}</h5>
@@ -23,9 +23,16 @@ window.addEventListener('DOMContentLoaded', async () => {
         const response  = await fetch(url);
 
         if (!response.ok) {
-            console.log("Response not ok");
+            const html = `
+            <div class="alert alert-danger d-flex align-items-center" role="alert">
+                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                <div>Response is not ok</div>
+            </div>`
+            const column = document.querySelector('.row');
+            column.innerHTML += html;
         } else {
             const data = await response.json();
+            
             for (let conference of data.conferences) {
                 const detailUrl = `http://localhost:8000${conference.href}`;
                 const detailResponse = await fetch(detailUrl);
@@ -42,15 +49,21 @@ window.addEventListener('DOMContentLoaded', async () => {
                     let start = (date1.getMonth()+1) + "/" + (date1.getDate()) + "/" + (date1.getFullYear());
                     let end = (date2.getMonth()+1) + "/" + (date2.getDate()) + "/" + (date2.getFullYear());
                     const html = createCard(name, description, pictureUrl, start, end, location);
-                    const column3 = document.querySelector('.row');
-                    column3.innerHTML += html;
+                    const column = document.querySelector('.row');
+                    column.innerHTML += html;
                     }
                 }
             
         
             }
     } catch (e) {
-        console.log("error:", e)
+        const html = `
+        <div class="alert alert-danger d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+            <div>Error: ${e}</div>
+        </div>`
+        const column = document.querySelector('.row');
+        column.innerHTML += html;
     }
 
 });
